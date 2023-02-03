6e7862aef1912a2baace53200ae901868ddd8c36
William E Hahn <williamedwardhahn@gmail.com>
2023-02-03 00:44:28 -0500

    Create Hahn_Imports.py

---
+++ Hahn_Imports.py
@@ -0,0 +1,195 @@
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
+from IPython.display import Image as PyImage
+import matplotlib.pyplot as plt
+import imageio
+import torch
+import torchvision
+from torchvision import models, transforms
+import numpy as np
+from torchvision.models import *
+from PIL import Image
+import requests
+from torchsummary import summary
+from skimage.util import montage
+from torch.autograd import Variable
+from skimage import io as skio
+from scipy import signal
+
+def take_photo(filename='photo.jpg', quality=0.8):
+  js = Javascript('''
+    async function takePhoto(quality) {
+      const div = document.createElement('div');
+      const capture = document.createElement('button');
+      capture.textContent = 'Capture';
+      div.appendChild(capture);
+
+      const video = document.createElement('video');
+      video.style.display = 'block';
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+      document.body.appendChild(div);
+      div.appendChild(video);
+      video.srcObject = stream;
+      await video.play();
+
+      // Resize the output to fit the video element.
+      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+      // Wait for Capture to be clicked.
+      await new Promise((resolve) => capture.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+      div.remove();
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+    ''')
+  display(js)
+  data = eval_js('takePhoto({})'.format(quality))
+  binary = b64decode(data.split(',')[1])
+  with open(filename, 'wb') as f:
+    f.write(binary)
+  return filename
+  
+  
+  
+  from IPython.display import display, Javascript
+from IPython.display import Image as PyImage
+from google.colab.output import eval_js
+import html
+from base64 import b64decode, b64encode
+import cv2
+import numpy as np
+import PIL
+import io
+def jsob_to_image(js_object):
+  # decode base64 image
+  image_bytes = b64decode(js_object.split(',')[1])
+  # convert bytes to numpy array
+  img_array = np.frombuffer(image_bytes, dtype=np.uint8)
+  # convert numpy array into OpenCV BGR 
+  frame = cv2.imdecode(img_array, flags=1)
+
+  return frame
+def video_stream():
+  js = Javascript('''
+    var video;
+    var div = null;
+    var stream;
+    var captureCanvas;
+    var imgElement;
+    var labelElement;
+    
+    var pendingResolve = null;
+    var shutdown = false;
+    
+    function removeDom() {
+       stream.getVideoTracks()[0].stop();
+       video.remove();
+       div.remove();
+       video = null;
+       div = null;
+       stream = null;
+       imgElement = null;
+       captureCanvas = null;
+       labelElement = null;
+    }
+    
+    function onAnimationFrame() {
+      if (!shutdown) {
+        window.requestAnimationFrame(onAnimationFrame);
+      }
+      if (pendingResolve) {
+        var result = "";
+        if (!shutdown) {
+          captureCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
+          result = captureCanvas.toDataURL('image/jpeg', 0.8)
+        }
+        var lp = pendingResolve;
+        pendingResolve = null;
+        lp(result);
+      }
+    }
+    
+    async function createDom() {
+      if (div !== null) {
+        return stream;
+      }
+
+      div = document.createElement('div');
+      div.style.border = '2px solid black';
+      div.style.padding = '3px';
+      div.style.width = '100%';
+      div.style.maxWidth = '600px';
+      document.body.appendChild(div);
+      
+           
+      video = document.createElement('video');
+      video.style.display = 'block';
+      video.width = div.clientWidth - 6;
+      video.setAttribute('playsinline', '');
+      video.onclick = () => { shutdown = true; };
+      stream = await navigator.mediaDevices.getUserMedia(
+          {video: { facingMode: "environment"}});
+      div.appendChild(video);
+
+      imgElement = document.createElement('img');
+      imgElement.style.position = 'absolute';
+      imgElement.style.zIndex = 1;
+      imgElement.onclick = () => { shutdown = true; };
+      div.appendChild(imgElement);
+      
+      const instruction = document.createElement('div');
+      instruction.innerHTML = 
+          '<span style="blue: red; font-weight: bold;">' +
+          'click here to stop the video</span>';
+      div.appendChild(instruction);
+      instruction.onclick = () => { shutdown = true; };
+      
+      video.srcObject = stream;
+      await video.play();
+
+      captureCanvas = document.createElement('canvas');
+      captureCanvas.width = 640; 
+      captureCanvas.height = 480; 
+      window.requestAnimationFrame(onAnimationFrame);
+      
+      return stream;
+    }
+    async function stream_frame() {
+      if (shutdown) {
+        removeDom();
+        shutdown = false;
+        return '';
+      }
+
+      var preCreate = Date.now();
+      stream = await createDom();
+      
+      var preShow = Date.now();
+  
+            
+
+      
+      var preCapture = Date.now();
+      var result = await new Promise(function(resolve, reject) {
+        pendingResolve = resolve;
+      });
+      shutdown = false;
+      
+      return {'create': preShow - preCreate, 
+              'show': preCapture - preShow, 
+              'capture': Date.now() - preCapture,
+              'img': result};
+    }
+    ''')
+
+  display(js)
+def video_frame():
+  data = eval_js('stream_frame()')
+  return data
